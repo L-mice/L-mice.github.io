a7a461890a71cac92db5bcf8a9a85af6e3bf054f
かずま <51999502+kazumax7@users.noreply.github.com>
2020-02-21 11:23:14 +0900

    Update html.js

--- scripts/html.js
+++ scripts/html.js
@@ -1,1 +1,19 @@
+initialSetting();
 
+function initialSetting(){
+  let unicode = document.createElement("meta");
+  unicode.charset = "utf-8";
+  
+  let viewport = document.createElement("meta");
+  viewport.name = "viewport";
+  viewport.content = [
+    "width=device-width",
+    "initial-scale=1.0",
+    "minimum-scale=1.0",
+    "maximum-scale=1.0",
+    "user-scalable='no'",
+  ].join(";");
+  
+  document.head.appendChild(unicode);
+  document.head.appendChild(viewport);
+}
