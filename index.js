d3abecfea66c2663534b40e1613ee1c65f5d0a37
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-04 22:25:05 +0900

    reset interface

--- index.js
+++ index.js
@@ -1,48 +1,4 @@
 const SUBTITLE = "Top";
 const INDEX_CSS = "index.css";
 
-const Menu_id = "items_back";
-const Menu_items = [
-    {
-        title: "≡",
-        activity: function(){
-            resetMenu();
-            for(let k = 1, back = document.getElementById(Menu_id); k < Menu_items.length; k++){
-                let item = makeMenuItem(Menu_items[k]);
-                
-                setTimeout(function(){
-                    back.appendChild(item);
-                },50);
-            }
-        },
-    },
-    {
-        title: "close",
-        activity: function(){
-            resetMenu();
-            
-            let item = makeMenuItem(Menu_items[0]);
-            item.style.width = "100vw";
-            item.style.height = "4vh";
-
-            document.getElementById(Menu_id).appendChild(item);
-        },
-    },
-];
-
 importCss(INDEX_CSS);
-Menu_items[1].activity();
-
-function makeMenuItem(obj){
-    let item = document.createElement("button");
-    item.innerHTML = obj.title;
-    item.onclick = obj.activity;
-    item.classList.add("menuItem");
-
-    return item;
-}
-
-function resetMenu(){
-    let back = document.getElementById(Menu_id);
-    back.innerHTML = "";
-}
