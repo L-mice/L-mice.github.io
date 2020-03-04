22235c731dd7d6518ce8589caf75759355a1a87f
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-05 05:34:48 +0900

    a

--- scripts/header.js
+++ scripts/header.js
@@ -32,7 +32,7 @@ const Header = {
         icon.classList.add(Header.CLASS_NAME.icon);
     
         let titleBar = document.createElement("p");
-        titleBar.innerHTML = SUBTITLE || "";
+        //titleBar.innerHTML = SUBTITLE;
         titleBar.classList.add(Header.CLASS_NAME.title);
     
         Header.item.leftside.appendChild(icon);
