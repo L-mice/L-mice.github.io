3bd2dd8b53045a15125a5c351653c2c51d9b53ad
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-10 23:39:50 +0900

    Test Banner activation

--- articles/test/unique.js
+++ articles/test/unique.js
@@ -1,3 +1,4 @@
 (function(){
     document.body.innerHTML = "<div class=\"test\"><p> Test Banner </p></div>" + document.body.innerHTML;
+    document.body.children[0].classList.add("activeBanner");
 })();
