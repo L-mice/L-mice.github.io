a4d6794d45b7e24a4604d4d275a525b690e420a6
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-10 23:20:18 +0900

    Test Banner
    エロサイトのうざいやつ

--- articles/test/unique.js
+++ articles/test/unique.js
@@ -1,2 +1,7 @@
 const SUBTITLE = "TEST";
 importCss("index.css");
+
+(function(){
+    let a = document.getElementsByClassName("contents")[0];
+    a.innerHTML += "<div class=\"test\"><p> Test Banner </p></div>";
+})();
