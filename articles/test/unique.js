c0c4651c43f144e99b623b5fbb40466ce6b1dbc2
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-10 23:25:40 +0900

    fix

--- articles/test/unique.js
+++ articles/test/unique.js
@@ -1,6 +1,3 @@
-const SUBTITLE = "TEST";
-importCss("index.css");
-
 (function(){
     let a = document.getElementsByClassName("contents")[0];
     a.innerHTML += "<div class=\"test\"><p> Test Banner </p></div>";
