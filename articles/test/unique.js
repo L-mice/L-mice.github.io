83df2427c12a808c0d220399c7df5e1292bc6fe7
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-10 23:47:29 +0900

    fix

--- articles/test/unique.js
+++ articles/test/unique.js
@@ -1,4 +1,8 @@
 (function(){
     document.body.innerHTML = "<div class=\"test\"><p> Test Banner </p></div>" + document.body.innerHTML;
-    document.body.children[0].classList.add("activeBanner");
+    
+    let banner = document.body.children[0];
+    banner.onclick = function(){
+        banner.className = (banner.className == "test") ? "test activeBanner" : "test";
+    };
 })();
