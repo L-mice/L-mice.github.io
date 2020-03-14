03ddc70eed45891b3d67bc08f5377e709fc6e6b9
かずま <51999502+kazumax7@users.noreply.github.com>
2020-03-15 07:31:16 +0900

    Update unique.js

--- articles/tools/click_counter/unique.js
+++ articles/tools/click_counter/unique.js
@@ -1,6 +1,6 @@
 const HIGH_SCORE_ID = "Click Counter Highscore";
 
-const countTime = 10000;
+const countTime = 3000;
 const filterId = "click_filter";
 const displayId = "count";
 
@@ -28,17 +28,17 @@ let highscore;
                 let clicks = countList[countList.length -1];
                 let sec = countTime/1000;
 
+                if(clicks/sec > highscore){
+                    highscore = clicks/sec;
+                    localStorage.setItem(HIGH_SCORE_ID, clicks/sec);
+                }
+
                 let d = makeDialogue(`count-${countList.length}`);
                 document.body.insertAdjacentElement("beforeend", d.getDialogue());
                 d.setTitle("Your score");
                 d.setMessage(`This time, you clicked ${clicks} times in ${sec} seconds. <br> This is ${clicks/sec} clicks per 1 second.<br><br> Your highest score is ${highscore}/s.`);
                 
-                display.innerText = "Click";
-
-                if(clicks/sec > highscore){
-                    highscore = clicks/sec;
-                    localStorage.setItem(HIGH_SCORE_ID, clicks/sec);
-                }
+                display.innerText = "Click";               
             }, countTime);
         }
         countList[countList.length -1]++;
