d3eb759514e472ec719356a9d79a9b0ea8b66645
かずま <51999502+kazumax7@users.noreply.github.com>
2020-04-09 18:54:31 +0900

    aa

--- articles/tools/school_timetable/unique.js
+++ articles/tools/school_timetable/unique.js
@@ -3,8 +3,38 @@ const timetable_name = "School Time Table";
 const classtime_list_name = "Class Time List";
 
 let subjects;
+/*
+    [
+        {
+            name: "subject name",
+            background: "table item background for this subject"
+        }
+    ]
+*/
+
 let timetable;
+/*
+    timetable[sunday, monday, - , saturday]
+    [
+        [
+            0,  //index of subject
+            2,
+            5,
+        ]
+    ]
+*/
+
 let classtime;
+/*
+    [
+        {
+            start: "8:50",
+            end: "9:40"
+        }
+    ]
+*/
+
+let tableitems = [];
 
 (function(){
     timetableDataSetup();
@@ -26,4 +56,35 @@ function timetableDataSave(){
 function timetableBuild(){
     let date = new Date();
     let day = date.getDay();
+
+    let classes = timetable[day];
+
+    for(let k = 0; k < classes.length; k++){
+        let subject = subjects[classes[k]];
+        let item = createTableItem(subject, k);
+        item.classList.add("nextday");
+        setTimeout(function(){
+            item.classList.remove("nextday")
+        }, 50);
+
+        tableitems.push(item);
+    }
+}
+
+function createTableItem(subject, time){
+    let back = document.createElement("div");
+    back.classList.add("item");
+    back.style.background = subject.background;
+
+    let start  = classtime[time].start;
+    let end = classtime[time].end;
+    let sub = subject.name;
+
+    let inside = document.createElement("div");
+    inside.classList.add("inside");
+    inside.innerText = ` ${time} ${start} - ${end} | ${sub}`;
+
+    back.appendChild(inside);
+
+    return back;
 }
