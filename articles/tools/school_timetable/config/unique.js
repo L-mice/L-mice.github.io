ff34dfa5f8e35b68dde80e9645b167ec2e4b2a62
かずま <51999502+kazumax7@users.noreply.github.com>
2020-04-11 02:53:04 +0900

    a

---
+++ articles/tools/school_timetable/config/unique.js
@@ -0,0 +1,45 @@
+const editSubjectBtnId = "editSubjectBtn";
+const editTimeBtnId = "editTimeBtn";
+const editTimeTableId = "editTimetableBtn";
+
+const editSubjectId = "subjectEditor";
+const editTimeId = "timeEditor";
+const editTimeTableId = "timetableEditor";
+
+const editSubjectClass = "editSubject";
+const editTimeClass = "editTime";
+const editTimeTableClass = "editTimeTable";
+
+const editorId = "editor";
+
+
+let editor;
+let editSubjectBtn;
+let editTimeBtn;
+let editTimetableBtn;
+
+
+(function(){
+    editor = document.getElementById(editorId);
+
+    editSubjectBtn = document.getElementById(editSubjectBtnId);
+    editSubjectBtn.onclick = function(){
+        editor.classList.add(editSubjectClass);
+        editor.classList.remove(editTimeClass);
+        editor.classList.remove(editTimeTableClass);
+    };
+
+    editTimeBtn = document.getElementById(editTimeBtnId);
+    editTimeBtn.onclick = function(){
+        editor.classList.remove(editSubjectClass);
+        editor.classList.add(editTimeClass);
+        editor.classList.remove(editTimeTableClass);
+    };
+
+    editTimeTableId = document.getElementById(editTimeTableId);
+    editTimetableBtn.onclick = function(){
+        editor.classList.remove(editSubjectClass);
+        editor.classList.remove(editTimeClass);
+        editor.classList.add(editTimeTableClass);
+    };
+})();
