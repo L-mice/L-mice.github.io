11398772a33bc1ac57e93e802124ada76e0b9fa1
かずま <51999502+kazumax7@users.noreply.github.com>
2020-04-19 00:07:10 +0900

    a

---
+++ articles/create_youtubechannel/unique.js
@@ -0,0 +1,6 @@
+(function(){
+    let btn = document.getElementById("toYouTube");
+    btn.onclick = function(){
+        window.open("https://www.youtube.com/channel/UCcaESRA_Rz4lqKZpanfftBQ");
+    };
+})();
